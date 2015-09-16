ec82a31eceed5569e891a3f4ff7222cecbb473d9
Mike Thorn <mike@TherapyCharts.com>
2015-09-16 16:35:21 -0400

    fixing paths, adding initial tag

---
+++ src/anchor.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace DevLucid\Masonry;
+
+class anchor extends tag
+{
+    function __construct()
+    {
+        
+    }
+}
---
+++ src/tag.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace DevLucid;
+namespace DevLucid\Masonry;
+
+class tag
+{
+    function __construct()
+    {
+
+    }
+
+    function __toString()
+    {
+        return '<b>Testing works</b>';
+    }
+}
